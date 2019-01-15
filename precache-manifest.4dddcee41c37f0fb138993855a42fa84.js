1332ff7e63ed0e8c5e12532c87f4e154880afda8
Alexandria Brown <axbn2190@gmail.com>
2019-01-14 16:58:39 -0800

    Updates

---
+++ precache-manifest.4dddcee41c37f0fb138993855a42fa84.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f81bd0fffbba4e71c667ec9011f83211",
+    "url": "/projects-portfolio/static/media/mediaranker.f81bd0ff.png"
+  },
+  {
+    "revision": "fc2d50f1ceb3958c5b06",
+    "url": "/projects-portfolio/static/css/main.7f384350.chunk.css"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/projects-portfolio/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "bc2e062b5222a849b7e5",
+    "url": "/projects-portfolio/static/js/runtime~main.bc2e062b.js"
+  },
+  {
+    "revision": "d48890fc78145f9eccc415aa8c91d713",
+    "url": "/projects-portfolio/static/media/projects.d48890fc.txt"
+  },
+  {
+    "revision": "f7c8f3d03d27c03ade79e64879c435c2",
+    "url": "/projects-portfolio/static/media/hangman.f7c8f3d0.png"
+  },
+  {
+    "revision": "15c83b84c9591fc3bfefdd8d4fc2baf3",
+    "url": "/projects-portfolio/static/media/js-scrabble.15c83b84.png"
+  },
+  {
+    "revision": "284a38685f0ebfb31293b80dec07e124",
+    "url": "/projects-portfolio/static/media/litterpatrol.284a3868.png"
+  },
+  {
+    "revision": "fc2d50f1ceb3958c5b06",
+    "url": "/projects-portfolio/static/js/main.fc2d50f1.chunk.js"
+  },
+  {
+    "revision": "716b15f826a2beb692329cbdbd9336b1",
+    "url": "/projects-portfolio/static/media/pickaflick.716b15f8.png"
+  },
+  {
+    "revision": "3f57bff5905334fe11842a423ea34feb",
+    "url": "/projects-portfolio/static/media/recipedia.3f57bff5.png"
+  },
+  {
+    "revision": "5e7d1297af206491a76571a04146c6ad",
+    "url": "/projects-portfolio/static/media/robot-chat.5e7d1297.png"
+  },
+  {
+    "revision": "57dfe4db45526a99c450bb5b5bf1c089",
+    "url": "/projects-portfolio/static/media/scrabble.57dfe4db.png"
+  },
+  {
+    "revision": "9e186a1f3dc2bd69cb745534716fc59e",
+    "url": "/projects-portfolio/static/media/sweetsy.9e186a1f.png"
+  },
+  {
+    "revision": "b8d14e39dfcebd1a64e07f715ce703d4",
+    "url": "/projects-portfolio/static/media/trek.b8d14e39.png"
+  },
+  {
+    "revision": "8d64d9ea0823e2a49fe94df88725e504",
+    "url": "/projects-portfolio/static/media/videostore.8d64d9ea.png"
+  },
+  {
+    "revision": "ff566379099009be984b81be4810da5b",
+    "url": "/projects-portfolio/index.html"
+  }
+];
