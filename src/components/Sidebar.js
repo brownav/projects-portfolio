ae49b3c9848237e23f98bde8a092f93f05f12699
Alexandria Brown <axbn2190@gmail.com>
2019-01-16 13:53:49 -0800

    adds custom icon tab image, removes unused react default icon, minor fixes to data

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,7 +18,7 @@ class Sidebar extends Component {
         <img className="profile" width="130em" height="150em" src={profile} alt="profile"></img><br></br>
         <sub>Hello! This is an in-progress snapshot of several projects. Some are deployed, others link to GitHub repos.</sub>
         <div className="icons">
-          <a href="https://drive.google.com/file/d/1McgEcEjh4SeRsRDJSLmZmLEX90XWee-3/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img width="50em" height="50em" src={resume} alt="resume"></img></a>
+          <a href="https://drive.google.com/file/d/1M8yKOlPMie_QXDgiYF3vehEKFurqDPE4/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img width="50em" height="50em" src={resume} alt="resume"></img></a>
           <a href="https://www.linkedin.com/in/alexandria-brown-47bab5ab/" target="_blank" rel="noopener noreferrer"><img width="50em" height="50em" src={linkedin} alt="linkedin"></img></a>
           <a href="https://github.com/brownav" target="_blank" rel="noopener noreferrer"><img width="50em" height="50em" src={github} alt="github"></img></a>
         </div>
