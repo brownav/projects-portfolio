86c1e77c72344f627aa557a2b7edbb91a7b7c0c1
Alexandria Brown <axbn2190@gmail.com>
2019-01-15 21:13:25 -0800

    changed profile border and capitalized title

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,7 +10,7 @@ class Sidebar extends Component {
     return (
       <section>
         <h1>Alexandria Brown</h1>
-        <h3>Full-stack software developer</h3>
+        <h3>Full-stack Software Developer</h3>
         <img className="profile" width="130em" height="150em" src={profile} alt="profile"></img><br></br>
         <sub>Hello! This is an in-progress snapshot of my projects. Some are deployed, others link to GitHub repos. Feel free to browse more projects on my <a href="https://github.com/brownav" target="_blank" rel="noopener noreferrer">Github</a>.</sub>
       </section>
