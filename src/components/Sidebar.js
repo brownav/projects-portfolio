47e9ffadcd8f93a53e8f777f035cc52cbc8bf4a7
Alexandria Brown <axbn2190@gmail.com>
2019-01-15 21:49:01 -0800

    adds icons with links to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,12 +7,21 @@ class Sidebar extends Component {
 
   render() {
     let profile = (image('./profile.png'));
+    let github = (image('./github-icon.png'));
+    let linkedin = (image('./linkedin-icon.png'));
+    let resume = (image('./resume.png'));
+
     return (
       <section>
         <h1>Alexandria Brown</h1>
         <h3>Full-stack Software Developer</h3>
         <img className="profile" width="130em" height="150em" src={profile} alt="profile"></img><br></br>
-        <sub>Hello! This is an in-progress snapshot of my projects. Some are deployed, others link to GitHub repos. Feel free to browse more projects on my <a href="https://github.com/brownav" target="_blank" rel="noopener noreferrer">Github</a>.</sub>
+        <sub>Hello! This is an in-progress snapshot of several projects. Some are deployed, others link to GitHub repos.</sub>
+        <div className="icons">
+          <a href="https://drive.google.com/file/d/1McgEcEjh4SeRsRDJSLmZmLEX90XWee-3/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img width="50em" height="50em" src={resume} alt="resume"></img></a>
+          <a href="https://www.linkedin.com/in/alexandria-brown-47bab5ab/" target="_blank" rel="noopener noreferrer"><img width="50em" height="50em" src={linkedin} alt="linkedin"></img></a>
+          <a href="https://github.com/brownav" target="_blank" rel="noopener noreferrer"><img width="50em" height="50em" src={github} alt="github"></img></a>
+        </div>
       </section>
     );
   }
