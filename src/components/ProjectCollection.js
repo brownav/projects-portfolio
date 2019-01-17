e956dd66050b5f06264d8b6b7e1b7eedd847be4c
Alexandria Brown <30912601+brownav@users.noreply.github.com>
2019-01-17 12:08:55 -0800

    revert file spacing from 8 to 2

--- src/components/ProjectCollection.js
+++ src/components/ProjectCollection.js
@@ -7,44 +7,44 @@ const images = require.context('../../public/images', true);
 
 class ProjectCollection extends Component {
   constructor(props) {
-		super(props)
+    super(props)
 
-		this.state = {
-			projects: []
-		}
-	}
+    this.state = {
+      projects: []
+    }
+  }
 	
-	componentDidMount = () => {
-		fetch(data)
-		.then( resp => resp.text())
-		.then( resp => this.setState({projects: JSON.parse(resp)}))
-		.catch( err => console.log(err))
-	}
+  componentDidMount = () => {
+    fetch(data)
+    .then( resp => resp.text())
+    .then( resp => this.setState({projects: JSON.parse(resp)}))
+    .catch( err => console.log(err))
+  }
 		 
-	renderProjects = () => {
-		let projects = this.state.projects.map((elem, index) => {
-			let screenshot = (images(elem.picture));
-			return (
-				<Project
-					key={index}
-					title={elem.title}
-					summary={elem.summary}
-					picture={screenshot}
-					link={elem.link}
-					tags={elem.tags}
-				/>
-			)
-		});
-		return projects;
-	}
+  renderProjects = () => {
+    let projects = this.state.projects.map((elem, index) => {
+      let screenshot = (images(elem.picture));
+        return (
+          <Project
+            key={index}
+            title={elem.title}
+            summary={elem.summary}
+            picture={screenshot}
+            link={elem.link}
+            tags={elem.tags}
+          />
+        )
+    });
+    return projects;
+  }
 
-	render() {
-		return (
-			<div className="container">
-				{this.renderProjects()}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        {this.renderProjects()}
+      </div>
+    );
+  }
 }
 
 export default ProjectCollection;
